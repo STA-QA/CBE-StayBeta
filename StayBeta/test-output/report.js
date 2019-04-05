3c2e0563f7a83790b4efaa46d86aebfa8922b302
Adams, Kiranmai <kiranmai.adams@statravel.com>
2019-04-05 15:07:06 +0100

    Update report.js

--- StayBeta/test-output/report.js
+++ StayBeta/test-output/report.js
@@ -32,3 +32,230 @@ formatter.step({
 formatter.match({
   "location": "LoginDefinitions.User_launches_the_application()"
 });
+formatter.result({
+  "duration": 19020455161,
+  "error_message": "org.openqa.selenium.WebDriverException: chrome not reachable\n  (Session info: chrome\u003d73.0.3683.86)\n  (Driver info: chromedriver\u003d2.46.628402 (536cd7adbad73a3783fdc2cab92ab2ba7ec361e1),platform\u003dWindows NT 10.0.16299 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:25:53\u0027\nSystem info: host: \u0027UKLF3GWKC2\u0027, ip: \u002710.44.253.139\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_202\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, acceptSslCerts: false, applicationCacheEnabled: false, browserConnectionEnabled: false, browserName: chrome, chrome: {chromedriverVersion: 2.46.628402 (536cd7adbad73a..., userDataDir: C:\\Users\\KIRANM~1.ADA\\AppDa...}, cssSelectorsEnabled: true, databaseEnabled: false, goog:chromeOptions: {debuggerAddress: localhost:64093}, handlesAlerts: true, hasTouchScreen: false, javascriptEnabled: true, locationContextEnabled: true, mobileEmulationEnabled: false, nativeEvents: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, proxy: Proxy(), rotatable: false, setWindowRect: true, strictFileInteractability: false, takesHeapSnapshot: true, takesScreenshot: true, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unexpectedAlertBehaviour: ignore, unhandledPromptBehavior: ignore, version: 73.0.3683.86, webStorageEnabled: true}\nSession ID: 1a1b03b736dffec7be82031dfb19e6b7\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:214)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:166)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:80)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:44)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.get(RemoteWebDriver.java:277)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver$RemoteNavigation.to(RemoteWebDriver.java:857)\r\n\tat utilities.TestBase.openUrl(TestBase.java:29)\r\n\tat stepDefinitions.LoginDefinitions.User_launches_the_application(LoginDefinitions.java:18)\r\n\tat ✽.Given User launches the application(src/test/java/testCaseResources/DeleteShoppingCart.feature:5)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "LoginDefinitions.user_enters_valid_credentials()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.match({
+  "location": "LoginDefinitions.user_logged_into_application()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenario({
+  "line": 10,
+  "name": "User Navigates to Hotel Page",
+  "description": "",
+  "id": "delete-the-product-from-the-shopping-cart;user-navigates-to-hotel-page",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 12,
+  "name": "User Selects any agency",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 13,
+  "name": "Click on Hotel Menu",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "TestCase616.User_Selects_any_agency()"
+});
+formatter.result({
+  "duration": 6383134958,
+  "error_message": "org.openqa.selenium.WebDriverException: chrome not reachable\n  (Session info: chrome\u003d73.0.3683.86)\n  (Driver info: chromedriver\u003d2.46.628402 (536cd7adbad73a3783fdc2cab92ab2ba7ec361e1),platform\u003dWindows NT 10.0.16299 x86_64) (WARNING: The server did not provide any stacktrace information)\nCommand duration or timeout: 0 milliseconds\nBuild info: version: \u00273.141.59\u0027, revision: \u0027e82be7d358\u0027, time: \u00272018-11-14T08:25:53\u0027\nSystem info: host: \u0027UKLF3GWKC2\u0027, ip: \u002710.44.253.139\u0027, os.name: \u0027Windows 10\u0027, os.arch: \u0027amd64\u0027, os.version: \u002710.0\u0027, java.version: \u00271.8.0_202\u0027\nDriver info: org.openqa.selenium.chrome.ChromeDriver\nCapabilities {acceptInsecureCerts: false, acceptSslCerts: false, applicationCacheEnabled: false, browserConnectionEnabled: false, browserName: chrome, chrome: {chromedriverVersion: 2.46.628402 (536cd7adbad73a..., userDataDir: C:\\Users\\KIRANM~1.ADA\\AppDa...}, cssSelectorsEnabled: true, databaseEnabled: false, goog:chromeOptions: {debuggerAddress: localhost:64093}, handlesAlerts: true, hasTouchScreen: false, javascriptEnabled: true, locationContextEnabled: true, mobileEmulationEnabled: false, nativeEvents: true, networkConnectionEnabled: false, pageLoadStrategy: normal, platform: XP, platformName: XP, proxy: Proxy(), rotatable: false, setWindowRect: true, strictFileInteractability: false, takesHeapSnapshot: true, takesScreenshot: true, timeouts: {implicit: 0, pageLoad: 300000, script: 30000}, unexpectedAlertBehaviour: ignore, unhandledPromptBehavior: ignore, version: 73.0.3683.86, webStorageEnabled: true}\nSession ID: 1a1b03b736dffec7be82031dfb19e6b7\n*** Element info: {Using\u003dxpath, value\u003d//td[2]/a/span}\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance0(Native Method)\r\n\tat sun.reflect.NativeConstructorAccessorImpl.newInstance(NativeConstructorAccessorImpl.java:62)\r\n\tat sun.reflect.DelegatingConstructorAccessorImpl.newInstance(DelegatingConstructorAccessorImpl.java:45)\r\n\tat java.lang.reflect.Constructor.newInstance(Constructor.java:423)\r\n\tat org.openqa.selenium.remote.ErrorHandler.createThrowable(ErrorHandler.java:214)\r\n\tat org.openqa.selenium.remote.ErrorHandler.throwIfResponseFailed(ErrorHandler.java:166)\r\n\tat org.openqa.selenium.remote.http.JsonHttpResponseCodec.reconstructValue(JsonHttpResponseCodec.java:40)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:80)\r\n\tat org.openqa.selenium.remote.http.AbstractHttpResponseCodec.decode(AbstractHttpResponseCodec.java:44)\r\n\tat org.openqa.selenium.remote.HttpCommandExecutor.execute(HttpCommandExecutor.java:158)\r\n\tat org.openqa.selenium.remote.service.DriverCommandExecutor.execute(DriverCommandExecutor.java:83)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.execute(RemoteWebDriver.java:552)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:323)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElementByXPath(RemoteWebDriver.java:428)\r\n\tat org.openqa.selenium.By$ByXPath.findElement(By.java:353)\r\n\tat org.openqa.selenium.remote.RemoteWebDriver.findElement(RemoteWebDriver.java:315)\r\n\tat CBE.StayBeta.HomePageImpl.UserSelectsAgency(HomePageImpl.java:22)\r\n\tat stepDefinitions.TestCase616.User_Selects_any_agency(TestCase616.java:25)\r\n\tat ✽.Given User Selects any agency(src/test/java/testCaseResources/DeleteShoppingCart.feature:12)\r\n",
+  "status": "failed"
+});
+formatter.match({
+  "location": "HomePageDefinitions.Click_on_Hotel_Menu()"
+});
+formatter.result({
+  "status": "skipped"
+});
+formatter.scenarioOutline({
+  "line": 15,
+  "name": "User Book a hotel through \u003cData Source\u003e",
+  "description": "",
+  "id": "delete-the-product-from-the-shopping-cart;user-book-a-hotel-through-\u003cdata-source\u003e",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 17,
+  "name": "User is in the Hotel Search Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "Search for a \u003cCountry\u003e with particular \u003cCountrySearchString\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "Select the \u003cStart Day\u003e and \u003cDuration Of Stay\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "Select Exclude dorm beds",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "Select Number of \u003cRooms\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "Select Hotel \u003cData Source\u003e",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "Click Search Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "Select a Random Hotel",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "Clicks on Add to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "Click on Delete button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "Product should be deleted from cart",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 30,
+  "name": "",
+  "description": "",
+  "id": "delete-the-product-from-the-shopping-cart;user-book-a-hotel-through-\u003cdata-source\u003e;",
+  "rows": [
+    {
+      "cells": [
+        "Country",
+        "CountrySearchString",
+        "Start Day",
+        "Duration Of Stay",
+        "Rooms",
+        "Data Source",
+        "Guests"
+      ],
+      "line": 31,
+      "id": "delete-the-product-from-the-shopping-cart;user-book-a-hotel-through-\u003cdata-source\u003e;;1"
+    },
+    {
+      "cells": [
+        "Buenos Aires (BUE), Argentina (AR)",
+        "BUE",
+        "2",
+        "1",
+        "2",
+        "Tourico",
+        "2"
+      ],
+      "line": 32,
+      "id": "delete-the-product-from-the-shopping-cart;user-book-a-hotel-through-\u003cdata-source\u003e;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.scenario({
+  "line": 32,
+  "name": "User Book a hotel through Tourico",
+  "description": "",
+  "id": "delete-the-product-from-the-shopping-cart;user-book-a-hotel-through-\u003cdata-source\u003e;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline"
+});
+formatter.step({
+  "line": 17,
+  "name": "User is in the Hotel Search Page",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 18,
+  "name": "Search for a Buenos Aires (BUE), Argentina (AR) with particular BUE",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 19,
+  "name": "Select the 2 and 1",
+  "matchedColumns": [
+    2,
+    3
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 20,
+  "name": "Select Exclude dorm beds",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 21,
+  "name": "Select Number of 2",
+  "matchedColumns": [
+    4
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 22,
+  "name": "Select Hotel Tourico",
+  "matchedColumns": [
+    5
+  ],
+  "keyword": "And "
+});
+formatter.step({
+  "line": 23,
+  "name": "Click Search Button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 24,
+  "name": "Select a Random Hotel",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 25,
+  "name": "Clicks on Add to cart",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 26,
+  "name": "Click on Delete button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 27,
+  "name": "Product should be deleted from cart",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "HotelBookingDefinitions.user_is_in_the_hotel_search_page()"
+});
