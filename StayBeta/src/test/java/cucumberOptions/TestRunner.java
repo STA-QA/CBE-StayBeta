5ccc8ce83532611f4b4c820086688d9b28b699d2
Adams, Kiranmai <kiranmai.adams@statravel.com>
2019-04-08 15:44:26 +0100

    changed testrunner file

--- StayBeta/src/test/java/cucumberOptions/TestRunner.java
+++ StayBeta/src/test/java/cucumberOptions/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/testCaseResources/HotelBooking7.feature", 
+@CucumberOptions(features = "src/test/java/testCaseResources", 
 glue = "stepDefinitions",  plugin = {"pretty","json:target/cucumber-report/cucumber.json"})
 
 public class TestRunner extends AbstractTestNGCucumberTests {
