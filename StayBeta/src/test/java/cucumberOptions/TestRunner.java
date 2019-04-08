dba895050fbe5bb6f895f3f828a0904d4c125053
Adams, Kiranmai <kiranmai.adams@statravel.com>
2019-04-08 12:28:16 +0100

    t

--- StayBeta/src/test/java/cucumberOptions/TestRunner.java
+++ StayBeta/src/test/java/cucumberOptions/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/testCaseResources", 
+@CucumberOptions(features = "src/test/java/testCaseResources/DeleteShoppingCart.feature", 
 glue = "stepDefinitions",  plugin = {"pretty","json:target/cucumber-report/cucumber.json"})
 
 public class TestRunner extends AbstractTestNGCucumberTests {
