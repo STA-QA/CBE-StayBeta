c47bc2d77b36722790d795972d23d9e2ec2fb302
Adams, Kiranmai <kiranmai.adams@statravel.com>
2019-11-08 09:14:31 +0000

    amendment updation

--- StayBeta/src/main/java/CBE/StayBeta/RandomDataGenerationImpl.java
+++ StayBeta/src/main/java/CBE/StayBeta/RandomDataGenerationImpl.java
@@ -1,12 +1,21 @@
 package CBE.StayBeta;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import com.cucumber.listener.Reporter;
 
 import Base.BaseUtil;
+import gherkin.deps.net.iharder.Base64;
 import stayBetaInterfaces.RandomDataGeneration;
 
 
@@ -32,10 +41,38 @@ public class RandomDataGenerationImpl extends BaseUtil implements RandomDataGene
 
 	public void AddScreenshotToReport() throws IOException {
 		//captureScreenshot(CBEDriver, "Screenshot");
-		String screenShotPath = BaseUtil.screenshot(CBEDriver, System.currentTimeMillis());
-		Reporter.addScreenCaptureFromPath(screenShotPath);
+		
+		  String screenShotPath = BaseUtil.screenshot(CBEDriver,
+		  System.currentTimeMillis());
+		  Reporter.addScreenCaptureFromPath(screenShotPath);
+		 
+		
 	}
 
+	public static String getBase64Screenshot() throws IOException {
+
+	    Date oDate = new Date();
+	    SimpleDateFormat oSDF = new SimpleDateFormat("yyyyMMddHHmmss");
+	    String sDate = oSDF.format(oDate);
+	    String encodedBase64 = null;
+	    FileInputStream fileInputStream = null;
+	    TakesScreenshot screenshot = (TakesScreenshot) CBEDriver;
+	    File source = screenshot.getScreenshotAs(OutputType.FILE);
+	    String destination =System.getProperty("user.dir")+"\\Foldername\\target\\cucumber-reports\\"+"Screenshot_" + sDate + ".png";
+	    File finalDestination = new File(destination);
+	    FileUtils.copyFile(source, finalDestination);
+
+	    try {
+	        fileInputStream =new FileInputStream(finalDestination);
+	        byte[] bytes =new byte[(int)finalDestination.length()];
+	        fileInputStream.read(bytes);
+	        encodedBase64 = new String(Base64.encodeBytes(bytes));
+
+	    }catch (FileNotFoundException e){
+	        e.printStackTrace();
+	    }
+	    return "data:image/png;base64,"+encodedBase64;
+	}
 
 
 
