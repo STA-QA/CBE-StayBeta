e4b49e05277bb6d2253b102f82295eb71b848249
gururajsta <Gururaj.Subash@statravel.com>
2019-12-04 14:11:15 +0000

    added code to rerun failed test cases

--- StayBeta/src/test/java/stepDefinitions/Hook.java
+++ StayBeta/src/test/java/stepDefinitions/Hook.java
@@ -31,7 +31,7 @@ public class Hook extends BaseUtil {
 		options.setExperimentalOption("useAutomationExtension", false);
 		options.setPageLoadStrategy(PageLoadStrategy.NORMAL);
 		options.addArguments("start-maximized");
-		options.addArguments("----headless");
+		//options.addArguments("----headless");
 		CBEDriver = new ChromeDriver(options);
 		CBEDriver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 
