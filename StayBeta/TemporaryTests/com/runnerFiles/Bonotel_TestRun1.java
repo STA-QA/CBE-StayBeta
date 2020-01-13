4ab737b58929f32fef254a2a5809b6f478500992
Adams, Kiranmai <kiranmai.adams@statravel.com>
2020-01-13 10:18:54 +0000

    Added runner files for bonotel and AA

---
+++ StayBeta/TemporaryTests/com/runnerFiles/Bonotel_TestRun1.java
@@ -0,0 +1,42 @@
+package com.runnerFiles;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import com.github.mkolisnyk.cucumber.reporting.CucumberResultsOverview;
+import Base.funtions;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions(features = "TemporaryTests/bonotel_TestRun1", plugin = {
+		"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:", "rerun:target/rerun.txt",
+		"json:target/cucumber-reports/cucumber.json" }, glue = "stepDefinitions")
+public class Bonotel_TestRun1 extends AbstractTestNGCucumberTests {
+
+	public static String timeStamp = new SimpleDateFormat("yyyy-MMM-dd HH:mm:ss")
+			.format(Calendar.getInstance().getTime());
+
+	@BeforeSuite
+	public static void setup() {
+
+		funtions commonfunctions = new funtions();
+		commonfunctions.reportfunction();
+	}
+
+	@AfterSuite(alwaysRun = true)
+	public void setupp() {
+
+		CucumberResultsOverview results = new CucumberResultsOverview();
+		results.setOutputDirectory("target");
+		results.setOutputName("cucumber-results");
+		results.setSourceFile("target/cucumber-reports/cucumber.json");
+		try {
+			results.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
