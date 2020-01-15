de6552446e55be4dd57d579c6f4e4ed8f9edd96e
Adams, Kiranmai <kiranmai.adams@statravel.com>
2020-01-15 16:18:24 +0000

    Changed test runner file for bonotel

--- StayBeta/src/test/java/cucumberOptions/TestRunner.java
+++ StayBeta/src/test/java/cucumberOptions/TestRunner.java
@@ -10,7 +10,7 @@ import Base.funtions;
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
 
-@CucumberOptions(features = "src/test/java/arabianAdventures_TestRun1", plugin = {
+@CucumberOptions(features = "src/test/java/bonotel_TestRun1", plugin = {
 		"com.vimalselvam.cucumber.listener.ExtentCucumberFormatter:", "rerun:target/rerun.txt",
 		"json:target/cucumber-reports/cucumber.json" }, glue = "stepDefinitions")
 public class TestRunner extends AbstractTestNGCucumberTests {
